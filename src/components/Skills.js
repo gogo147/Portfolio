e0eda3cde2355b335841521f4493a170c70d340d
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2022-12-21 22:45:00 +0100

    added updates

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,7 +13,7 @@ const Skills = () => {
        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
            <div>
                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
-               <p className='py-4'>These are the technologies that I'va worked with</p>
+               <p className='py-4'>These are the technologies that I have worked with</p>
            </div>
 
            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-6 text-center py-8'>
