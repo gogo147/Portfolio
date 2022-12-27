c70abe6804a0f7238b311b41423168c6175eb4d1
gogo147 <94003065+gogo147@users.noreply.github.com>
2022-12-27 22:25:45 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export const Home = () => {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-pink-600 text-4xl'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>George Manaz</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front/App Developer.</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Fullstack Developer.</h2>
         </div>
     </div>
   )
